75d05fb53edf58c2d01cd12df0b5d7c4b835f008
Zandrr <alexander.campbell@colorado.edu>
2015-09-22 10:52:11 -0600

    created basic http server

---
+++ code/Server.scala
@@ -0,0 +1,15 @@
+import com.twitter.finagle.{Httpx, Service}
+import com.twitter.finagle.httpx
+import com.twitter.util.{Await, Future}
+
+object Server extends App {
+    val  service = new Service[ httpx.Request, httpx.Response ] {
+        def apply(req: httpx.Request) : Future[httpx.Response] = {
+            Future.value (
+                httpx.Response(req.version, httpx.Status.Ok)
+            )
+        }
+    }
+val server = Httpx.serve(":8080", service)
+Await.ready(server)
+}
---
+++ code/build.sbt
@@ -0,0 +1,5 @@
+name := "quickstart"
+
+version := "1.0"
+
+libraryDependencies += "com.twitter" %% "finagle-httpx" % "6.28.0"
